a7e2bbb1901f1444448a2de54f1d0d9e34edf5c1
Esteban Guzman <esteban.d.guzman@gmail.com>
2023-09-05 22:44:41 -0500

    inicial

---
+++ src/setting-voice.js
@@ -0,0 +1,10 @@
+module.exports = {
+	host: 'https://c4f4-2806-2f0-8080-fbd1-5e6-6738-8da8-bf20.ngrok-free.app',
+	welcome: "Welcome to the ivr demo",
+	option: "Press 1 for Spanish. Press 2 for English",
+	notSelect: "Sorry, I didn't catch that. Please hang up and try again",
+	errorSelect: "Sorry, that's not a valid input",
+	operationEnglishStart: 'please how can we help you',
+	operationEnglish: 'thank you, it will be answered shortly',
+	operationSpanish: 'Rediregiendo a operador en español'
+}
